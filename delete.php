4433a738cd6e87524b967bbde22b41a49e88de1c
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-15 13:01:14 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,22 +1,13 @@
 <?php
-// include connection
-include 'db_connection.php';
+// include database connection file
+include_once("config.php");
 
-// check if url contain id, if not redirect to index page
-if (isset($_GET["idStudent"]) && !empty(trim($_GET["idStudent"]))) {
-    // get id from url
-    $id = trim($_GET["idStudent"]);
+// Get id from URL to delete that user
+$id = $_GET['id'];
 
-    $sql = "DELETE FROM students WHERE idStudent='$id'";
+// Delete user row from table based on given id
+$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
 
-    if (mysqli_query($conn, $sql)) {
-        echo "<script>alert('Record deleted successfully');</script>";
-        echo "<script>window.location.href='http://localhost/PHP-MySQL/';</script>";
-        exit();
-    }
-    // close connection
-    mysqli_close($conn);
-} else {
-    echo "<script>alert('Please select record to delete');</script>";
-    echo "<script>window.location.href='http://localhost/PHP-MySQL/';</script>";
-}
+// After delete redirect to Home, so that latest user list will be displayed.
+header("Location:index.php");
+?>
