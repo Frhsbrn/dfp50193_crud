4a1a58c45c80eeb823a8409966849dcf42d8e2a4
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-15 13:01:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,75 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+// Create database connection using config file
+include_once("config.php");
 
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
-    <title>Display Data - PHP CRUD</title>
+    <title>Homepage</title>
 </head>
 
 <body>
-    <div class="container mt-5">
-        <a href="create.php" class="btn btn-secondary"><i class="fas fa-plus-circle"></i> Add Record</a>
-        <table class="table table-bordered table-striped mt-4">
-            <caption>Records of Students</caption>
-            <thead>
-                <tr>
-                    <th>SN</th>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Email Address</th>
-                    <th>Course</th>
-                    <th>Batch</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Operations</th>
-                </tr>
-            </thead>
+<a href="add.php">Add New User</a><br/><br/>
 
-            <tbody>
-                <?php
-                // include connection
-                include 'db_connection.php';
+    <table width='80%' border=1>
 
-                $sql = "SELECT * FROM students";
-                $result = mysqli_query($conn, $sql);
-                $records = mysqli_num_rows($result);
-
-                if ($records > 0) {
-                    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                    $count = 1;
-                    foreach ($row as $row) { ?>
-                        <tr>
-                            <td><?= $count++; ?></td>
-                            <td class="text-capitalize"><?= $row["firstname"]; ?></td>
-                            <td class="text-capitalize"><?= $row["lastname"]; ?></td>
-                            <td><?= $row["email"]; ?></td>
-                            <td class="text-uppercase"><?= $row["course"]; ?></td>
-                            <td><?= $row["batch"]; ?></td>
-                            <td class="text-capitalize"><?= $row["city"]; ?></td>
-                            <td class="text-capitalize"><?= $row["state"]; ?></td>
-                            <td>
-                                <a href="update.php?id=<?= $row['idStudent']; ?>" class="btn btn-primary"><i class="far fa-edit"></i> Update</a>&nbsp;
-                                <a href="delete.php?id=<?= $row["idStudent"]; ?>" class=" btn btn-danger" onclick="return checkDelete();"><i class="far fa-trash-alt"></i> Delete</a>
-                            </td>
-                        </tr>
-                <?php
-                    }
-                }
-                mysqli_close($conn);
-                ?>
-            </tbody>
-        </table>
-    </div>
-    <script>
-        function checkDelete() {
-            return confirm("Are you sure you want to delete this record?");
-        }
-    </script>
+    <tr>
+        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
+    </tr>
+    <?php
+    while($user_data = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['mobile']."</td>";
+        echo "<td>".$user_data['email']."</td>";
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";
+    }
+    ?>
+    </table>
 </body>
-
 </html>
