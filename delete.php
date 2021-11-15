d8bd4a58081e432eb7689c4cf56490d7f140ac46
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-15 12:38:21 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,11 +3,11 @@
 include 'db_connection.php';
 
 // check if url contain id, if not redirect to index page
-if (isset($_GET["id"]) && !empty(trim($_GET["id"]))) {
+if (isset($_GET["idStudent"]) && !empty(trim($_GET["idStudent"]))) {
     // get id from url
-    $id = trim($_GET["id"]);
+    $id = trim($_GET["idStudent"]);
 
-    $sql = "DELETE FROM students WHERE id='$id'";
+    $sql = "DELETE FROM students WHERE idStudent='$id'";
 
     if (mysqli_query($conn, $sql)) {
         echo "<script>alert('Record deleted successfully');</script>";
