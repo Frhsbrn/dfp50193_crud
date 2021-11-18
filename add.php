b8def4673a058c8ed7c2a9baa8a21fc0f1ff8839
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-18 08:38:39 +0800

    Update add.php

--- add.php
+++ add.php
@@ -13,10 +13,9 @@
 		$nama = mysqli_real_escape_string($mysqli, $_POST['nama']);
 		$umur = mysqli_real_escape_string($mysqli, $_POST['umur']);
 		$email = mysqli_real_escape_string($mysqli, $_POST['email']);
-		$filename = $_FILES['gambar']['name'];
-
+		
 		// CEK DATA TIDAK BOLEH KOSONG
-		if (empty($nama) || empty($umur) || empty($email) || empty($filename)) {
+		if (empty($nama) || empty($umur) || empty($email)) {
 
 			if (empty($nama)) {
 				echo "<font color='red'>Kolom Nama tidak boleh kosong.</font><br/>";
@@ -30,23 +29,14 @@
 				echo "<font color='red'>Kolom Email tidak boleh kosong.</font><br/>";
 			}
 
-			if (empty($filename)) {
-				echo "<font color='red'>Kolom Gambar tidak boleh kosong.</font><br/>";
-			}
-
 			// KEMBALI KE HALAMAN SEBELUMNYA
 			echo "<br/><a href='javascript:self.history.back();'>Kembali</a>";
 		} else {
-			// JIKA SEMUANYA TIDAK KOSONG
-			$filetmpname = $_FILES['gambar']['tmp_name'];
-
-			// FOLDER DIMANA GAMBAR AKAN DI SIMPAN
-			$folder = 'image/';
-			// GAMBAR DI SIMPAN KE DALAM FOLDER
-			move_uploaded_file($filetmpname, $folder . $filename);
+			
+			
 
 			// MEMASUKAN DATA DATA + NAMA GAMBAR KE DALAM DATABASE
-			$result = mysqli_query($mysqli, "INSERT INTO users(nama,umur,email,gambar) VALUES('$nama', '$umur', '$email', '$filename')");
+			$result = mysqli_query($mysqli, "INSERT INTO users(nama,umur,email) VALUES('$nama', '$umur', '$email')");
 
 			// MENAMPILKAN PESAN BERHASIL
 			echo "<font color='green'>Data Berhasil ditambahkan.";
