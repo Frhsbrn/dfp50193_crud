9a07035db9e170e3fd541a106280d7a320f3bec3
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-18 08:56:20 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,11 @@
 <?php
 $databaseHost = 'localhost';
-$databaseName = 'database'; // NAMA DATABASE
-$databaseUsername = 'root'; // USERNAME DATABASE
-$databasePassword = ''; //PASSWORD DATABASE
+$databaseName = 'database'; 
+$databaseUsername = 'root'; 
+$databasePassword = ''; 
 
-// MENGHUBUNGKAN DATABASE
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-// JIKA DATABASE TIDAK ADA ATAU TIDAK TERSAMBUNG
 if (mysqli_connect_errno()) {
     printf("%s \n", mysqli_connect_error());
     exit();
