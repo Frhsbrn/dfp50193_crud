64744ea6628cfe3b1e7aee23a1082984909f8272
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-18 08:56:30 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,24 +1,16 @@
 <?php
-// INCLUDE KONEKSI KE DATABASE
+
 include_once("config.php");
 
 if (isset($_POST['update'])) {
 
-	// AMBIL ID DATA
+
 	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
-
-	// AMBIL NAMA FILE FOTO SEBELUMNYA
-	$data = mysqli_query($mysqli, "SELECT gambar FROM users WHERE id='$id'");
-	$dataImage = mysqli_fetch_assoc($data);
-
-
-	// AMBIL DATA DATA DIDALAM INPUT
 	$nama = mysqli_real_escape_string($mysqli, $_POST['nama']);
 	$umur = mysqli_real_escape_string($mysqli, $_POST['umur']);
 	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
-	$filename = $_FILES['newImage']['name'];
 
-	// CEK DATA TIDAK BOLEH KOSONG
+
 	if (empty($nama) || empty($umur) || empty($email)) {
 
 		if (empty($nama)) {
@@ -33,42 +25,28 @@ if (isset($_POST['update'])) {
 			echo "<font color='red'>Kolom Email tidak boleh kosong.</font><br/>";
 		}
 	} else {
+	
 
-		// JIKA FOTO DI GANTI
-		if (!empty($filename)) {
-			$filetmpname = $_FILES['newImage']['tmp_name'];
-			$folder = "image/";
-
-			// GAMBAR LAMA DI DELETE
-			unlink($folder . $oldImage) or die("GAGAL");
-
-			// GAMBAR BARU DI MASUKAN KE FOLDER
-			move_uploaded_file($filetmpname, $folder . $filename);
-
-			// NAMA FILE FOTO + DATA YANG DI GANTIBARU DIMASUKAN
-			$result = mysqli_query($mysqli, "UPDATE users SET nama='$nama',umur='$umur',email='$email',gambar='$filename' WHERE id=$id");
+			$result = mysqli_query($mysqli, "UPDATE users SET nama='$nama',umur='$umur',email='$email' WHERE id=$id");
 		}
 
-		// MEMASUKAN DATA YANG DI UPDATE KECUALI GAMBAR
 		$result = mysqli_query($mysqli, "UPDATE users SET nama='$nama',umur='$umur',email='$email' WHERE id=$id");
 
-		// REDIRECT KE HALAMAN INDEX.PHP
+
 		header("Location: index.php");
 	}
 }
 ?>
 <?php
-// AMBIL ID DARI URL
+
 $id = $_GET['id'];
 
-// AMBIL DATA BERDASARKAN ID
 $result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
 
 while ($res = mysqli_fetch_array($result)) {
 	$name = $res['nama'];
 	$age = $res['umur'];
 	$email = $res['email'];
-	$image = $res['gambar'];
 }
 ?>
 <html>
@@ -85,15 +63,15 @@ while ($res = mysqli_fetch_array($result)) {
 		<form name="form1" method="post" action="edit.php" enctype="multipart/form-data">
 			<table border="0">
 				<tr>
-					<td>Nama</td>
+					<td>NAME</td>
 					<td><input type="text" name="nama" value="<?php echo $name; ?>"></td>
 				</tr>
 				<tr>
-					<td>Umur</td>
+					<td>AGE</td>
 					<td><input type="text" name="umur" value="<?php echo $age; ?>"></td>
 				</tr>
 				<tr>
-					<td>Email</td>
+					<td>EMAIL</td>
 					<td><input type="text" name="email" value="<?php echo $email; ?>"></td>
 				</tr>
 				<tr>
