0bd2cd578e1752549d334c299f74d914c6b3a675
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-11-18 08:30:43 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,21 @@
 <?php
-// include database connection file
-include_once("config.php");
+// INCLUDE KONEKSI KE DATABASE
+include("config.php");
 
-// Get id from URL to delete that user
+// AMBIL DATA ID DI URL
 $id = $_GET['id'];
 
-// Delete user row from table based on given id
+// AMBIL NAMA FILE FOTO SEBELUMNYA
+$data = mysqli_query($mysqli, "SELECT gambar FROM users WHERE id='$id'");
+$dataImage = mysqli_fetch_assoc($data);
+$oldImage = $dataImage['gambar'];
+
+// DELETE GAMBAR LAMA
+$link = "image/" . $oldImage;
+unlink($link);
+
+// DELETE DATA DARI TABLE
 $result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
 
-// After delete redirect to Home, so that latest user list will be displayed.
+// REDIRECT KE index.php
 header("Location:index.php");
-?>
